e61f0ce48f89c8e61c1c821953e6e5a265c1ab72
Taylor Foster <tagfoster90@gmail.com>
2015-02-17 19:05:00 -0500

    leaving class, ends partner coding with @jaredstevick until tomorrow

--- js/main.js
+++ js/main.js
@@ -1,26 +1,42 @@
 $(document).ready(function(){
     $('#fullpage').fullpage();
-	$.getJSON('weather.json', function(data) {
+	$.getJSON('http://api.forecast.io/forecast/9e3f9effa091da78070dd0e26d01368f/28.4158,-81.2989', function(data) {
     //current weather
     var cTemp = data.currently.apparentTemperature;
     var cHumidity =  data.currently.humidity;
     var cSummary =  data.currently.summary;
     var cCloudCover =  data.currently.cloudCover;
-    var cIcon =  data.currently.icon;
-
-    // $('li:nth-child(1)').html(currentTemp + "\&#176");
-    // $('li:nth-child(2)').html(summary);
-    // $('li:nth-child(3)').html("cloud cover: "+ cloudCover);
-    // $('li:nth-child(4)').html("humidity: " + humidity);
-    // $('li:nth-child(5)').html('today will be: '+ icon);
+    
+    if(cSummary == 'Mostly Cloudy'){
+        $('i').addClass('wi wi-cloudy');
+    }
+    if(cSummary == 'Light Rain'){
+        $('i').addClass('wi wi-sprinkle');
+    }
+    if(cSummary = 'Partly Cloudy'){
+        $('i').addClass('wi wi-cloud');
+    }
+    if(cSummary == 'Rain'){
+        $('i').addClass('wi wi-rain');
+    }
+    if(cSummary == 'Snow'){
+        $('i').addClass('wi wi-snow');
+    };
+    $('li:nth-child(1)').html(cTemp + "\&#176");
+    $('li:nth-child(2)').html(cSummary);
+    $('li:nth-child(3)').html("cloud cover: "+ cCloudCover);
+    $('li:nth-child(4)').html("humidity: " + cHumidity);
 
     //hourly
     var hourly = data.hourly.data
     var hTemp =  _.map(hourly, 'temperature');
+    var hSummary = _.map(hourly, 'summary');
+    var hTime = _.map(hourly, 'time');
 
     for(var i=0; i < 24; i++){
-        $('.hour').append("<li>"+Math.floor(hTemp[i])+"\&#176"+"</li>");
-         }
-
+        var date = new Date(hTime[i] * 1000);
+        var hours = date.getHours();
+        $('.hour').append("<li>"+hours+":00"+" "+Math.floor(hTemp[i])+"\&#176"+ " " + hSummary[i] +"</li>");
+        }
    });
 });
